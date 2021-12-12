fe60405701bd259bf35265500c56319bd43bd39e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-12-12 23:00:36 +0100

    Test del método recuento()

---
+++ Conway/src/test/java/com/jaureguialzo/MainTest.java
@@ -0,0 +1,23 @@
+package com.jaureguialzo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    // Posición inicial del tablero (6x25)
+    private int[][] tablero = new int[][]{
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,},
+            {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0,},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+    };
+
+    @Test
+    void recuentoCorrecto() {
+        assertEquals(Main.recuento(tablero, 3, 2), 2);
+    }
+}
